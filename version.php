82c40a8a7e21adb970bef39933f39020da0ccb2b
Carlos Alexandre S. da Foneca <bozohhot@hotmail.com>
2013-02-18 11:24:54 -0300

    Issue #3 Wrong required version on module

--- version.php
+++ version.php
@@ -5,13 +5,13 @@
  *
  * @package    mod
  * @subpackage simplecertificate
- * @copyright  Carlos Fonseca <carlos.alexandre@outlook.com>, Mark Nelson <mark@moodle.com.au>
+ * @copyright  Carlos Alexandre S. da Fonseca <carlos.alexandre@outlook.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or late
  */
 
-$module->version  = 2013021500;  // The current module version (Date: YYYYMMDDXX)
-$module->requires =  2012112900;  // Requires this Moodle version (moodle 2.3.x)
+$module->version  = 2013021800;  // The current module version (Date: YYYYMMDDXX)
+$module->requires =  2012110900;  // Requires this Moodle version (moodle 2.4.x)
 $module->cron     = 4 * 3600;    // Period for cron to check this module (secs)
 $module->component = 'mod_simplecertificate';
-$module->release  = '1.2.1';       // Human-friendly version name
+$module->release  = '1.2.2';       // Human-friendly version name
 $module->maturity = MATURITY_STABLE;
