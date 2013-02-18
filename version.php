94df9d0e2316f22b1dacd5f21d64416da51845e3
Carlos Alexandre S. da Foneca <bozohhot@hotmail.com>
2013-02-18 11:36:17 -0300

    Issue #3 Wrong required version on module

--- version.php
+++ version.php
@@ -10,7 +10,7 @@
  */
 
 $module->version  = 2013021800;  // The current module version (Date: YYYYMMDDXX)
-$module->requires =  2012110900;  // Requires this Moodle version (moodle 2.4.x)
+$module->requires = 2012110900;  // Requires this Moodle version (moodle 2.4.x)
 $module->cron     = 4 * 3600;    // Period for cron to check this module (secs)
 $module->component = 'mod_simplecertificate';
 $module->release  = '1.2.2';       // Human-friendly version name
