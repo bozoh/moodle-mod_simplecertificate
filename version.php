36ba7e6bda741c45db99208986d6cebc96de9722
Carlos Alexandre S. da Foneca <bozohhot@hotmail.com>
2013-02-19 16:15:08 -0300

    Issue #4

--- version.php
+++ version.php
@@ -9,17 +9,9 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or late
  */
 
-<<<<<<< HEAD
 $module->version  = 2013021900;  // The current module version (Date: YYYYMMDDXX)
 $module->requires =  2012062500;  // Requires this Moodle version (moodle 2.3.x)
 $module->cron     = 4 * 3600;    // Period for cron to check this module (secs)
 $plugin->component = 'mod_simplecertificate';
 $module->release  = '1.1.4';       // Human-friendly version name
-=======
-$module->version  = 2013021500;  // The current module version (Date: YYYYMMDDXX)
-$module->requires =  2012062500;  // Requires this Moodle version (moodle 2.3.x)
-$module->cron     = 4 * 3600;    // Period for cron to check this module (secs)
-$plugin->component = 'mod_simplecertificate';
-$module->release  = '1.1.3';       // Human-friendly version name
->>>>>>> c0e1e4678fd90b59ff26a51d1d7a0995b07b4ea9
 $module->maturity = MATURITY_STABLE;
