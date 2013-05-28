56ccb6afd55097abbb5208dff4c22f1ab1f2e214
Carlos Alexandre S. da Foneca <bozohhot@hotmail.com>
2013-05-28 18:59:21 -0300

    Isseu #13

--- version.php
+++ version.php
@@ -9,9 +9,9 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or late
  */
 
-$module->version  = 2013021900;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2013052800;  // The current module version (Date: YYYYMMDDXX)
 $module->requires = 2012062500;  // Requires this Moodle version (moodle 2.3.x)
 $module->cron     = 4 * 3600;    // Period for cron to check this module (secs)
 $plugin->component = 'mod_simplecertificate';
-$module->release  = '1.1.4';       // Human-friendly version name
+$module->release  = '1.1.5';       // Human-friendly version name
 $module->maturity = MATURITY_STABLE;
