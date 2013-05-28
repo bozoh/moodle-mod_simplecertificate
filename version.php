8de36713c7437ee64f228e7693ede5e971b5914e
Carlos Alexandre S. da Foneca <bozohhot@hotmail.com>
2013-05-28 19:00:48 -0300

    Issue#13

--- version.php
+++ version.php
@@ -9,8 +9,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or late
  */
 
-$module->version  = 2013021900;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2013052800;  // The current module version (Date: YYYYMMDDXX)
 $module->requires = 2010112400;  // Requires this Moodle version
 $module->cron     = 4 * 3600;    // Period for cron to check this module (secs)
-$module->release  = '1.0.2';       // Human-friendly version name
+$module->release  = '1.0.3';       // Human-friendly version name
 $module->maturity = MATURITY_STABLE;
