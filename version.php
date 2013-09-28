38d054dbc2ad697fcf76ea2343afee4ffc6f1173
Carlos Alexandre S. da Fonseca <bozohhot@hotmail.com>
2013-09-28 01:47:36 -0300

    v.2.1.1

--- version.php
+++ version.php
@@ -13,6 +13,6 @@ $module->version  = 2013092700;  // The current module version (Date: YYYYMMDDXX
 $module->requires = 2013040500;  // Requires this Moodle version (moodle 2.5.x)
 $module->cron     = 4 * 3600;    // Period for cron to check this module (secs)
 $module->component = 'mod_simplecertificate';
-$module->release  = '2.1.0';       // Human-friendly version name
+$module->release  = '2.1.1';       // Human-friendly version name
 //MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC, MATURITY_STABLE
 $module->maturity = MATURITY_STABLE;
