949deeeffbc2dfc417345c31661c37cc54846722
Carlos Alexandre S. da Fonseca <bozohhot@hotmail.com>
2013-11-16 11:49:40 -0200

    fixing small bug

--- locallib.php
+++ locallib.php
@@ -1270,7 +1270,7 @@ class simplecertificate {
 				true);
 		
 		$mimetype = $fileinfo['mimetype'];
-		$output = '<img src="'.$OUTPUT->pix_url(file_mimetype_icon($mimetype)).'" height="16" width="16" alt="'.mimetype.'" />&nbsp;'.
+		$output = '<img src="'.$OUTPUT->pix_url(file_mimetype_icon($mimetype)).'" height="16" width="16" alt="'.$mimetype.'" />&nbsp;'.
 					'<a href="'.$link->out(false).'" target="_blank" >'.s($fileinfo['filename']).'</a>';
 	
 		$output .= '<br />';
