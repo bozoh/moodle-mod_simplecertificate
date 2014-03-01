f3613bd95a1c5f1000a47e084d99998f966392ea
Carlos Alexandre S. da Fonseca <bozohhot@hotmail.com>
2014-03-01 10:56:31 -0300

    close #66

--- version.php
+++ version.php
@@ -5,12 +5,11 @@
  *
  * @package    mod
  * @subpackage simplecertificate
- * @author	   Carlos Alexandre S. da Fonseca
- * @copyright  2013 - Carlos Alexandre S. da Fonseca
+ * @copyright  Carlos Alexandre S. da Fonseca <carlos.alexandre@outlook.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or late
  */
 
-$module->version  = 2013112500;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2014030100;  // The current module version (Date: YYYYMMDDXX)
 $module->requires = 2013040500;  // Requires this Moodle version (moodle 2.5.x)
 $module->cron     = 4 * 3600;    // Period for cron to check this module (secs)
 $module->component = 'mod_simplecertificate';
