8867355c2829030f5220348ffae2b4c90dfeb550
Carlos Alexandre S. da Foneca <bozohhot@hotmail.com>
2015-01-16 15:13:56 -0200

    v2.8RC (2015011600)

--- version.php
+++ version.php
@@ -13,11 +13,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2014070500;  // The current module version (Date: YYYYMMDDXX)
-$plugin->requires = 2014041100;  // Requires this Moodle version (moodle 2.7.x)
+$plugin->version  = 2015011600;  // The current module version (Date: YYYYMMDDXX)
+$plugin->requires = 2014101000;  // Requires this Moodle version (moodle 2.8.x)
 $plugin->cron     = 4 * 3600;    // Period for cron to check this module (secs)
 $plugin->component = 'mod_simplecertificate';
 $plugin->dependencies = array();
-$plugin->release  = '2.2.0';       // Human-friendly version name
+$plugin->release  = '2.2.1';       // Human-friendly version name
 //MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC, MATURITY_STABLE
-$plugin->maturity = MATURITY_STABLE;
+$plugin->maturity = MATURITY_RC;
