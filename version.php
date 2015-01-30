fe78af277b3b4ed272d12f3d2460675497a1a34c
Carlos Alexandre S. da Fonseca <bozohhot@hotmail.com>
2015-01-30 18:17:16 -0200

    v2.2.1 build 2015013026

--- version.php
+++ version.php
@@ -11,12 +11,13 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2015011600;  // The current module version (Date: YYYYMMDDXX)
+// The current module version (Date: YYYYMMDDXX where XX is the moodle version)
+$plugin->version  = 2015013026;  
 $plugin->requires = 2013101800;  // Requires this Moodle version (moodle 2.6.x)
 $plugin->cron     = 4 * 3600;    // Period for cron to check this module (secs)
 $plugin->component = 'mod_simplecertificate';
 $plugin->release  = '2.2.1';       // Human-friendly version name
 //MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC, MATURITY_STABLE
-$plugin->maturity = MATURITY_RC;
+$plugin->maturity = MATURITY_STABLE;
 
 $plugin->dependencies = array();
