f981e6a647646f25529b3aa84f6d6d0538315733
Carlos Alexandre S. da Fonseca <bozohhot@hotmail.com>
2015-06-17 10:15:26 -0300

    v2.2 - 2015061727

--- version.php
+++ version.php
@@ -13,7 +13,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2014070100;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version  = 2015061727;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2014041100;  // Requires this Moodle version (moodle 2.7.x)
 $plugin->cron     = 4 * 3600;    // Period for cron to check this module (secs)
 $plugin->component = 'mod_simplecertificate';
