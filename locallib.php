9c364af44d58f791ba2c3c84ff8e5b06e78fa26f
Carlos Alexandre S. da Fonseca <bozohhot@hotmail.com>
2017-12-05 09:53:45 -0200

    Remove UUID lib #183

--- locallib.php
+++ locallib.php
@@ -818,17 +818,16 @@ class simplecertificate {
     }
 
     /**
-     * Generate a version 1 UUID (time based)
+     * Generate a UUID 
      * you can verify the generated code in:
      * http://www.famkruithof.net/uuid/uuidgen?typeReq=-1
      *
-     * @return string UUID_v1
+     * @return string UUID
      */
     protected function get_issue_uuid() {
         global $CFG;
-        require_once($CFG->dirroot . '/mod/simplecertificate/lib/lib.uuid.php');
-        $uuid = UUID::mint(UUID::VERSION_1, self::CERTIFICATE_COMPONENT_NAME);
-        return $uuid->__toString();
+        require_once($CFG->libdir . '/horde/framework/Horde/Support/Uuid.php');
+        return (string)new Horde_Support_Uuid();
     }
 
     /**
