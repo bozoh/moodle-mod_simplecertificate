c6ebd14889ec2c810de214f2f463d0692239021d
Carlos Alexandre S. da Fonseca <bozohhot@hotmail.com>
2018-08-21 13:38:41 -0300

    Update verion 2.2.8

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version  = 2018080400;  // The current module version (Date: YYYYMMDDXX).
+$plugin->version  = 2018082000;  // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2018051700;  // Requires this Moodle version (moodle 3.5.x).
 $plugin->cron     = 4 * 3600;    // Period for cron to check this module (secs).
 $plugin->component = 'mod_simplecertificate';
